202bea3a69a0917f4ebdfb834641f3b99908dfea
Franco Villafañe <FrancoMiguel.Villafane@alumnos.uai.edu.ar>
2022-10-15 14:40:31 -0300

    Primera

--- Parcial.py
+++ Parcial.py
@@ -1,8 +1,12 @@
+
+from ast import Not
+from pickle import LIST
 import numpy as np
 import datetime as dt
 from enum import Enum
 
 
+ListaUsuarios = []
 class Usuario:
     def __init__(self, tipo, numero, nombre, apellido):
         self.tipo = tipo
@@ -11,7 +15,10 @@ class Usuario:
         self.apellido = apellido
     def __str__(self):
         return f"{self.nombre} {self.apellido}" 
-
+persona = Usuario("DNI", 123, "asd", "sad")
+ListaUsuarios.append(persona)
+persona = Usuario("DNI", 122, "assasd", "sad")
+ListaUsuarios.append(persona)
 class Tarjeta:
     def __init__(self, fechaotorga):
         self.fechaotorgacion = fechaotorga
@@ -55,7 +62,49 @@ def CrearUsuario():
     apellido = str(input())
 
     nuevo = Usuario(tipo, numero, nombre, apellido)
+    if len(ListaUsuarios)!=0:
+        if ValidarID(nuevo.numero):
+            ListaUsuarios.append(nuevo)
+            print (f"{nuevo} se ha agregado al sistema")
+            MenuPrincipal()
+        else:
+            print("El usuario ya existe.")
+            CrearUsuario()
+    else:
+        ListaUsuarios.append(nuevo)
+        print (f"{nuevo} se ha agregado al sistema\n")
+        MenuPrincipal()
 
-  ListaUsuarios = 
 
-CrearUsuario()
+def ValidarID(numero):
+    sino = True
+    for dni in ListaUsuarios:
+            if dni.numero == numero:
+                sino = False
+    return sino
+
+def BuscarIndex(numero):
+    for dni in ListaUsuarios:
+        if dni.numero == numero:
+            indice = ListaUsuarios.index(dni)
+    return indice
+
+def ModificarUsuario(modificado):
+
+    print("Ingrese Numero de documento:\n")
+    numero = int(input())
+    if not ValidarID(numero):
+        indice = BuscarIndex(numero)
+        ListaUsuarios.pop(indice)
+        ListaUsuarios.append(modificado)
+
+def EliminarUsuario():
+    print("Ingrese Numero de documento:\n")
+    numero = int(input())
+    if not ValidarID(numero):
+        indice = BuscarIndex(numero)
+        ListaUsuarios.pop(indice)
+     
+ModificarUsuario()
+for x in ListaUsuarios:
+    print(x)
