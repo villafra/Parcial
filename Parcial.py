34486eb8db164b1c5bbcae806ab719b5b6f73ba9
Franco Villafañe <FrancoMiguel.Villafane@alumnos.uai.edu.ar>
2022-10-16 09:22:04 -0300

    Usuario Lista, Tarjeta a medias. Falta lo demás

--- Parcial.py
+++ Parcial.py
@@ -1,5 +1,4 @@
 
-from email.policy import default
 import numpy as np
 import datetime as dt
 from Clases import *
@@ -85,7 +84,59 @@ def MenuTarjeta():
             MenuTarjeta()
     while menu2 <5:
         if menu2 == 1:
-            CrearTarjeta()
+            print("Buscar usuario por: (elija la opcion correcta)")
+            print("1-Apellido")
+            print("2-Nro Documento")
+            try:
+                respuesta = int(input())
+                if respuesta == 1:
+                    usuarios = BuscarUsuarioxNombre()
+                    if type(usuarios) != str:
+                        for usuario in usuarios:
+                            print(f"{usuario}, {usuario.tipo}:{usuario.numero}")
+                            usuario = BuscarUsuarioDNI()
+                            if type(usuario) != str:
+                                print(f"Se le va a asignar una tarjeta a {usuario}")
+                                print("Desea continuar?")
+                                print("1-Si")
+                                print("2-No")
+                            try:
+                                opcion = int(input())
+                                if opcion == 1:
+                                    CrearTarjeta(usuario)
+                                else:
+                                    MenuTarjeta()
+                            except:
+                                print("Debe elegir 1 o 2\n")
+                                MenuTarjeta()
+                    else:
+                        print(usuarios)
+                        MenuTarjeta()
+                elif respuesta == 2:
+                    usuario = BuscarUsuarioDNI()
+                    if type(usuario) != str:
+                        print(f"Se le va a asignar una tarjeta a {usuario}")
+                        print("Desea continuar?")
+                        print("1-Si")
+                        print("2-No")
+                        try:
+                            opcion = int(input())
+                            if opcion == 1:
+                                CrearTarjeta(usuario)
+                            else:
+                                MenuTarjeta()
+                        except:
+                            print("Debe elegir 1 o 2\n")
+                            MenuTarjeta()
+                    else:
+                        print(usuario)
+                        MenuTarjeta()
+                else:
+                    print("Debe elegir 1 o 2\n")
+                    MenuTarjeta()
+            except:
+                print("Debe elegir 1 o 2\n")
+                MenuTarjeta()
             break
         if menu2 == 2:
             ModificarLimite()
@@ -105,22 +156,30 @@ def CrearUsuario():
     print("Elija Tipo Documento:\n\
     1-DNI\n\
     2-Pasaporte\n")
-    tipo = TipoID(int(input())).name
+    try:
+        tipo = TipoID(int(input())).name
+    except :
+        print("Debe elegir 1 o 2\n")
+        CrearUsuario()
     print(f"Ingrese {tipo}:\n")
-    numero = int(input())
+    try:
+        numero = int(input())
+    except:
+        print(f"Ingrese {tipo} sin puntos ni comas\n")
+        CrearUsuario()
     print ("Ingrese Nombre:\n")
     nombre = str(input())
     print ("Ingrese Apellido:\n")
     apellido = str(input())
 
     nuevo = Usuario(tipo, numero, nombre, apellido)
-    if len(ListaUsuarios)!=0:
+    if ListaUsuarios:
         if ValidarID(nuevo.numero):
             ListaUsuarios.append(nuevo)
-            print (f"{nuevo} se ha agregado al sistema")
+            print (f"{nuevo} se ha agregado al sistema.\n")
             MenuCliente()
         else:
-            print("El usuario ya existe.")
+            print("El usuario ya existe.\n")
             CrearUsuario()
     else:
         ListaUsuarios.append(nuevo)
@@ -142,32 +201,138 @@ def BuscarIndex(numero):
     return indice
 
 def ModificarUsuario():
-    print("Ingrese Numero de documento:\n")
-    numero = int(input())
+    print("Ingrese Numero de documento de usuario a modificar:")
+    try:
+        numero = int(input())
+    except:
+        print(f"Ingrese documento sin puntos ni comas\n")    
     if not ValidarID(numero):
         indice = BuscarIndex(numero)
         aux = ListaUsuarios[indice]
         ListaUsuarios.pop(indice)
-        print("Elija Tipo Documento:\n\
-         1-DNI\n\
-         2-Pasaporte\n")
-        tipo = TipoID(int(input())).name
-        print(f"Ingrese {tipo}:\n")
-        numero = int(input())
-        print ("Ingrese Nombre:\n")
-        nombre = str(input())
-        print ("Ingrese Apellido:\n")
-        apellido = str(input())
+        print(f"Modificar tipo documento? Dato actual: {aux.tipo}")
+        print("1-Si")
+        print("2-No")
+        try:
+            respuesta = int(input())
+            if respuesta == 1:
+                print("Elija nuevo tipo documento:\n\
+                    1-DNI\n\
+                    2-Pasaporte\n")
+                try:
+                    tipo = TipoID(int(input())).name
+                except :
+                    print("Debe elegir 1 o 2\n")
+                    ListaUsuarios.insert(indice, aux)
+                    ModificarUsuario()
+            elif respuesta == 2:
+                tipo = aux.tipo
+            else:
+                print("Debe elegir 1 o 2\n")
+                ListaUsuarios.insert(indice, aux)
+                ModificarUsuario()
+        except:
+            ListaUsuarios.insert(indice, aux)
+            ModificarUsuario()
+        print(f"Modificar numero documento? Dato actual: {aux.numero}")
+        print("1-Si")
+        print("2-No")
+        try:
+            respuesta = int(input())
+            if respuesta == 1:
+                print(f"Ingrese nuevo {tipo}:")
+                try:
+                    numero = int(input())
+                except :
+                    print(f"Ingrese {tipo} sin puntos ni comas\n")
+                    ListaUsuarios.insert(indice, aux)
+                    ModificarUsuario()
+            elif respuesta == 2:
+                numero = aux.numero
+            else:
+                print("Debe elegir 1 o 2\n")
+                ListaUsuarios.insert(indice, aux)
+                ModificarUsuario()
+
+        except:
+            ListaUsuarios.insert(indice, aux)
+            ModificarUsuario()
+        print(f"Modificar nombre? Dato actual: {aux.nombre}")
+        print("1-Si")
+        print("2-No")
+        try:
+            respuesta = int(input())
+            if respuesta == 1:
+               print ("Ingrese nuevo Nombre:")
+               nombre = str(input())
+            elif respuesta == 2:
+                nombre = aux.nombre
+            else:
+                print("Debe elegir 1 o 2\n")
+                ListaUsuarios.insert(indice, aux)
+                ModificarUsuario()
+        except:
+            ListaUsuarios.insert(indice, aux)
+            ModificarUsuario()
+        print(f"Modificar Apellido? Dato actual: {aux.apellido}")
+        print("1-Si")
+        print("2-No")
+        try:
+            respuesta = int(input())
+            if respuesta == 1:
+                print ("Ingrese nuevo Apellido:")
+                apellido = str(input())
+            elif respuesta == 2:
+                apellido = aux.apellido
+            else:
+                print("Debe elegir 1 o 2\n")
+                ListaUsuarios.insert(indice, aux)
+                ModificarUsuario()
+        except:
+            ListaUsuarios.insert(indice, aux)
+            ModificarUsuario()
         modificado = Usuario(tipo, numero, nombre, apellido)
         ListaUsuarios.insert(indice,modificado)
+        print(f"El usuario {aux} ha sido modificado correctamente.")
+        print("Los nuevos datos son:")
+        print(f"{tipo}: {numero}")
+        print(modificado)
+        MenuCliente()
 
 def EliminarUsuario():
-    print("Ingrese Numero de documento:\n")
+    acumuladopesos = 0
+    acumuladodolares = 0
+    tarjetas = []
+    print("Ingrese Numero de documento de usuario a eliminar:")
     numero = int(input())
     if not ValidarID(numero):
         indice = BuscarIndex(numero)
-        ListaUsuarios.pop(indice)
-     
+        aux = ListaUsuarios[indice]
+        tarjetas = BuscarTarjeta(aux)
+        if tarjetas:
+            for tarjeta in tarjetas:
+                if tarjeta.AcumuladoPesos > 0:
+                    acumuladopesos += tarjeta.AcumuladoPesos
+                if tarjeta.AcumuladoDolares >0:
+                    acumuladodolares += tarjeta.AcumuladoDolares
+            if acumuladopesos <= 0 & acumuladodolares <= 0:
+                ListaUsuarios.pop(indice)
+                print(f"El usuario {aux} ha sido eliminado de la base de datos.")
+                MenuCliente()
+            else:
+                print(f"El usuario {aux} aun tiene tarjetas con deuda activa, no puede eliminarse.")
+                MenuCliente()
+        else:
+            ListaUsuarios.pop(indice)
+            print(f"El usuario {aux} ha sido eliminado de la base de datos.")
+            MenuCliente()
+def BuscarTarjeta(usuario):
+    tarjetasxpersonas = []
+    for tarjeta in ListaTarjetas:
+        if tarjeta.titular.numero == usuario.numero:
+            tarjetasxpersonas.append(tarjeta)
+    return tarjetasxpersonas
+            
 def GenerarNumero():
     nuevonumero = str(rnd.randint(0,9))
     for x in range(11):
@@ -180,29 +345,58 @@ def ValidarNumero(numero):
                 ValidarNumero(GenerarNumero())
     return numero
 
+def BuscarUsuarioDNI():
+    print(f"Ingrese numero de documento:")
+    try:
+       numero = int(input())
+    except :
+       print(f"Ingrese documento sin puntos ni comas\n")
+    for usuario in ListaUsuarios:
+        if usuario.numero == numero:
+            return usuario
+        else:
+            return "No se ha encontrado el usuario"
+
+def BuscarUsuarioxNombre():
+    usuarios = []
+    print(f"Ingrese apellido a buscar:")
+    apellido = str(input())
+    for usuario in ListaUsuarios:
+        if usuario.apellido == apellido:
+            usuarios.append(usuario)
+    if usuarios:
+        return usuarios
+    else:
+        return "No se ha encontrado usuario con ese apellido."
+
 def CrearTarjeta(usuario):
+    aux = usuario
     print("Elija Nivel Tarjeta:\n\
          1-Platinum\n\
          2-Gold\n\
          3-Plata\n")
-    tipo = TipoTarjeta(int(input()))
+    try:
+        tipo = TipoTarjeta(int(input()))
+    except :
+        print("Debe elegir de 1 a 3\n")
+        CrearTarjeta(aux)
     if tipo == TipoTarjeta.Platinum:
         tarjeta = Platinum(dt.date.today(), usuario)
         tarjeta.Setnumero(ValidarNumero(GenerarNumero()))    
     elif tipo == TipoTarjeta.Gold:
-        tarjeta = Gold(dt.date,usuario)
+        tarjeta = Gold(dt.date.today(),usuario)
         tarjeta.Setnumero(ValidarNumero(GenerarNumero()))
     elif tipo == TipoTarjeta.Plata:
-        tarjeta = Plata(dt.date, usuario)
+        tarjeta = Plata(dt.date.today(), usuario)
         tarjeta.Setnumero(ValidarNumero(GenerarNumero()))
     return tarjeta
 
 
 def ModificarLimite(numero):
-    print("Ingrese Nuevo Limite de Pesos:\n")
+    print("Ingrese Nuevo Limite de Pesos:")
     print("(0 para no modificar)")
     pesos = float(input())
-    print("Ingrese Nuevo Limite de Dolares:\n")
+    print("Ingrese Nuevo Limite de Dolares:")
     print("(0 para no modificar)")
     dolares = float(input())
     for tarjeta in ListaTarjetas:
